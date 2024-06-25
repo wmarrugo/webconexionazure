5e504ca85893bbc78d93b6b763b76fc0777cdf00
wmarrugo <wmarrugo@poligran.edu.co>
2024-06-24 23:21:14 -0500

    Update event-hub-reader.js

--- scripts/event-hub-reader.js
+++ scripts/event-hub-reader.js
@@ -2,31 +2,33 @@
  * Microsoft Sample Code - Copyright (c) 2020 - Licensed MIT
  */
 
-const { EventHubProducerClient, EventHubConsumerClient } = require('@azure/event-hubs');
+const { EventHubConsumerClient } = require('@azure/event-hubs');
 const { convertIotHubToEventHubsConnectionString } = require('./iot-hub-connection-string.js');
 
 class EventHubReader {
   constructor(iotHubConnectionString, consumerGroup) {
     this.iotHubConnectionString = iotHubConnectionString;
     this.consumerGroup = consumerGroup;
+    this.consumerClient = null;
   }
 
   async startReadMessage(startReadMessageCallback) {
     try {
       const eventHubConnectionString = await convertIotHubToEventHubsConnectionString(this.iotHubConnectionString);
-      const consumerClient = new EventHubConsumerClient(this.consumerGroup, eventHubConnectionString);
+      this.consumerClient = new EventHubConsumerClient(this.consumerGroup, eventHubConnectionString);
       console.log('Successfully created the EventHubConsumerClient from IoT Hub event hub-compatible connection string.');
 
-      const partitionIds = await consumerClient.getPartitionIds();
+      const partitionIds = await this.consumerClient.getPartitionIds();
       console.log('The partition ids are: ', partitionIds);
 
-      consumerClient.subscribe({
+      this.consumerClient.subscribe({
         processEvents: (events, context) => {
           for (let i = 0; i < events.length; ++i) {
             startReadMessageCallback(
               events[i].body,
               events[i].enqueuedTimeUtc,
-              events[i].systemProperties["iothub-connection-device-id"]);
+              events[i].systemProperties["iothub-connection-device-id"]
+            );
           }
         },
         processError: (err, context) => {
@@ -38,15 +40,17 @@ class EventHubReader {
     }
   }
 
-  // Close connection to Event Hub.
   async stopReadMessage() {
-    const disposeHandlers = [];
-    this.receiveHandlers.forEach((receiveHandler) => {
-      disposeHandlers.push(receiveHandler.stop());
-    });
-    await Promise.all(disposeHandlers);
-
-    this.consumerClient.close();
+    try {
+      if (this.consumerClient) {
+        await this.consumerClient.close();
+        console.log('EventHubConsumerClient closed.');
+      } else {
+        console.warn('No EventHubConsumerClient to close.');
+      }
+    } catch (ex) {
+      console.error('Error closing EventHubConsumerClient:', ex.message || ex);
+    }
   }
 }
 
